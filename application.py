2afd30798a5da1e48b946e435808f2d1f4c1a64f
IzzybellaDu <isabelladujiaen@gmail.com>
2022-01-13 13:24:03 +0000

    added Procfile

--- application.py
+++ application.py
@@ -12,7 +12,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-systemdb = SQL("sqlite:///system.db")
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
 
 units = ["kJ", "Calories"]
 
